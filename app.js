f6aec8332dbfa6c313994809c2527a3e0bb25f9b
Adeleke Hammed <lekhad19@gmail.com>
2024-01-27 18:15:24 +0100

    os-modules

--- app.js
+++ app.js
@@ -23,13 +23,40 @@
 // }, 1000);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
-const names = require('./4-names');
-const sayHi = require('./5-utils');
-const data = require('./6-alternative');
-require('./7-mind-grenade'); 
-console.log(names);
-sayHi('susan'); 
-// sayHi(john); 
-// sayHi(peter);
-sayHi(names.john);
-sayHi(names.peter);
+// const names = require('./4-names');
+// const sayHi = require('./5-utils');
+// const data = require('./6-alternative');
+// require('./7-mind-grenade'); 
+// console.log(names);
+// sayHi('susan'); 
+// // sayHi(john); 
+// // sayHi(peter);
+// sayHi(names.john);
+// sayHi(names.peter);
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+// const os = require('os');
+
+// // info about current user 
+// const user = os.userInfo();
+// console.log(user);
+
+// // method returns the system uptime in seconds 
+// console.log(`The System uptime is ${os.uptime()} seconds`);
+
+// const currentOS = {
+//     name: os.type(),  
+//     release:os.release(), 
+//     totalMem: os.totalmem(), 
+//     freeMem: os.freemem()
+// }
+
+// console.log(currentOS);
+
+////////////////////////////////////////////////////////////////////////////////////////////
+const path = require('path'); 
+console.log(path.sep);
+
+
+
+
