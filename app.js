657a66a7452240b3ff23db8084ea46fe56ce0c78
Adeleke Hammed <lekhad19@gmail.com>
2024-01-27 19:49:16 +0100

    asynchronous approach

--- app.js
+++ app.js
@@ -67,13 +67,41 @@
 // console.log(absolute);  
 
 /////////////////////////////////////////////////////////////////////////////////////////////
-const {readFileSync, writeFileSync} = require('fs');
+// const {readFileSync, writeFileSync} = require('fs');
 
-const first = readFileSync('./content/first.txt', 'utf8'); 
-const second = readFileSync('./content/second.txt', 'utf8'); 
+// const first = readFileSync('./content/first.txt', 'utf8'); 
+// const second = readFileSync('./content/second.txt', 'utf8'); 
 
-// console.log(first, second); 
-writeFileSync('./content/result-sync.text', 
-`Here is the result: ${first}, ${second}`, 
-{'flag' : 'a'}
-); 
+// // console.log(first, second); 
+// writeFileSync('./content/result-sync.text', 
+// `Here is the result: ${first}, ${second}`, 
+// {'flag' : 'a'}
+// ); 
+
+////////////////////////////////////////////////////////////////////////////////////////////
+const { readFile, writeFile } = require('fs'); 
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if(err) {
+        console.log(err);
+        return 
+    }
+    // console.log(result); 
+    const first = result; 
+    readFile('./content/second.txt', 'utf8', (err, result) => {
+        if(err) {
+            console.log(err);
+            return 
+        }
+        const second = result; 
+        writeFile('/content/result-async.txt',
+        `Here is the result : ${first}, ${second}`
+        , (err, result) => {
+            if(err) {
+                console.log(err)
+                return 
+            }
+            console.log(result);
+        })
+    })
+});
