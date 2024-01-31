9cfbf1faf475a360791103b32becb6465e760d79
Adeleke Hammed <lekhad19@gmail.com>
2024-01-31 10:54:12 +0100

    installed npm and used lodash

--- app.js
+++ app.js
@@ -107,23 +107,34 @@
 // });
 
 ////////////////////////////////////////////////////////////////////////////////////
-const http = require('http')
+// const http = require('http')
 
-const server = http.createServer((req, res) => {
-    // console.log(req);
-    if(req.url === '/'){
-        res.end('Welcome to our homepage')
-    }
+// const server = http.createServer((req, res) => {
+//     // console.log(req);
+//     if(req.url === '/'){
+//         res.end('Welcome to our homepage')
+//     }
 
-    if(req.url === '/about') {
-        res.end('Here is our short history')
-    }
+//     if(req.url === '/about') {
+//         res.end('Here is our short history')
+//     }
 
-    res.end(`
-    <h1>Oops!</h1>
-    <p>We can't seem to find the page you are looking for </p>
-    <a href="/">back</a>
-    `)
-});
+//     res.end(`
+//     <h1>Oops!</h1>
+//     <p>We can't seem to find the page you are looking for </p>
+//     <a href="/">back</a>
+//     `)
+// });
 
-server.listen(5000);
+// server.listen(5000);
+
+///////////////////////////////////////////////////////////////////////////////////
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
+const _ = require('lodash'); 
+
+const items = [1, 2, [3, [4]]]
+const newItems = _.flattenDeep(items); 
+console.log(newItems); 
