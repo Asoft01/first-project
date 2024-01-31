94ab6e6ce5f80d7339236c84e939a3f7914349e2
Adeleke Hammed <lekhad19@gmail.com>
2024-01-31 16:12:08 +0100

    added event loop

--- app.js
+++ app.js
@@ -133,8 +133,36 @@
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default)
 
-const _ = require('lodash'); 
+// const _ = require('lodash'); 
 
-const items = [1, 2, [3, [4]]]
-const newItems = _.flattenDeep(items); 
-console.log(newItems);
+// const items = [1, 2, [3, [4]]]
+// const newItems = _.flattenDeep(items); 
+// console.log(newItems);
+
+//////////////////////////////////////////////////////////////////////////////////
+// started operating system process
+// console.log('first');
+// setTimeout(() =>  {
+//     console.log('second');
+// }); 
+
+// console.log('third'); 
+// completed and exited operating system process 
+//////////////////////////////////////////////////////////////////////////////////
+// setInterval(() => {
+//     console.log('hello world');
+// }, 2000);
+
+// console.log(`I will run first`); 
+///////////////////////////////////////////////////////////////////////////////
+
+// const http = require('http'); 
+
+// const server = http.createServer((req, res) => {
+//     console.log('request event');
+//     res.end('hello world');
+// }); 
+
+// server.listen(5000, () => {
+//     console.log('Server listening on port : 5000...'); 
+// })
